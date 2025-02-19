7e4abf61374ddcdfa74de01ae57a52f364f61fbb
TomCube <121310847+yomcube@users.noreply.github.com>
2025-02-19 11:34:49 -0600

    Update installer.py

--- misc/installer.py
+++ misc/installer.py
@@ -1,7 +1,7 @@
 import os
 from requests import get
 
-from installer_backend import install
+from installer_backend import installer_backend
 
 def save_link_as_file(link, filepath, by=False):
     r = get(link, timeout=60)
@@ -10,6 +10,6 @@ def save_link_as_file(link, filepath, by=False):
 
 linkbase = "https://raw.githubusercontent.com/Tycho10101/LaunchiCube/refs/heads/main/"
 save_link_as_file(f"{linkbase}misc/installer_backend.py", "installer_backend.py")
-install()
+installer_backend.install()
 os.remove("installer_backend.py")
 os.remove("installer.py")
