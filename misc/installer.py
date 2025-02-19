a63bea18105f6725160e761caf3408213089be45
TomCube <121310847+yomcube@users.noreply.github.com>
2025-02-19 10:55:31 -0600

    Fix some linter errors

--- misc/installer.py
+++ misc/installer.py
@@ -1,10 +1,10 @@
-from requests import get
 import os
+from requests import get
 
-def save_link_as_file(link, filepath, bytes=False):
-    r = get(link)
-    with open(filepath, f"w{'b' if bytes else ''}") as f:
-        f.write(r.content if bytes else r.text)
+def save_link_as_file(link, filepath, by=False):
+    r = get(link, timeout=60)
+    with open(filepath, f"w{'b' if by else ''}") as f:
+        f.write(r.content if by else r.text)
 
 linkbase = "https://raw.githubusercontent.com/Tycho10101/LaunchiCube/refs/heads/main/"
 save_link_as_file(f"{linkbase}misc/installer_backend.py", "installer_backend.py")
