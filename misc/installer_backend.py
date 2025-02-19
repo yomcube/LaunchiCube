faa49e1f8fb61d6e8876f3883bae3b4468c5d02e
TomCube <121310847+yomcube@users.noreply.github.com>
2025-02-19 10:57:11 -0600

    Fix some linter errors

--- misc/installer_backend.py
+++ misc/installer_backend.py
@@ -8,7 +8,7 @@ class installer_backend:
         installer_backend.save_link_as_file(f"{linkbase}gui.py", "gui.py")
         installer_backend.save_link_as_file(f"{linkbase}utils.py", "utils.py")
         
-    def save_link_as_file(link, filepath, bytes=False):
-        r = get(link)
-        with open(filepath, f"w{'b' if bytes else ''}") as f:
-            f.write(r.content if bytes else r.text)
+    def save_link_as_file(link, filepath, by=False):
+        r = get(link, timeout=60)
+        with open(filepath, f"w{'b' if by else ''}") as f:
+            f.write(r.content if by else r.text)
