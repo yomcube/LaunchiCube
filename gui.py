97c6951b07190875d4e5c08fb20717793647c420
TomCube <121310847+yomcube@users.noreply.github.com>
2025-02-20 11:32:59 -0600

    Oops

--- gui.py
+++ gui.py
@@ -95,7 +95,7 @@ class Gui:
         def save_link_as_file(link, filepath):
             r = get(link, timeout=60)
             with open(filepath, "w", encoding="utf-8") as f:
-                f.write(r.content if by else r.text)
+                f.write(r.text)
 
         linkbase = "https://raw.githubusercontent.com/Tycho10101/LaunchiCube/refs/heads/main/"
         save_link_as_file(f"{linkbase}misc/installer_backend.py", "installer_backend.py")
