f2a433feccae27e12a94e7ce482460a325439705
TomCube <121310847+yomcube@users.noreply.github.com>
2025-02-24 11:47:45 -0600

    Don't use class for `installer_backend`

--- misc/installer_backend.py
+++ misc/installer_backend.py
@@ -1,9 +1,8 @@
 from utils import save_link_as_file
 
-class installer_backend:
-    def install():
-        linkbase = "https://raw.githubusercontent.com/Tycho10101/LaunchiCube/refs/heads/main/"
-        save_link_as_file(f"{linkbase}main.py", "main.py")
-        save_link_as_file(f"{linkbase}updater.py", "updater.py")
-        save_link_as_file(f"{linkbase}gui.py", "gui.py")
-        save_link_as_file(f"{linkbase}utils.py", "utils.py")
+def install():
+    linkbase = "https://raw.githubusercontent.com/Tycho10101/LaunchiCube/refs/heads/main/"
+    save_link_as_file(f"{linkbase}main.py", "main.py")
+    save_link_as_file(f"{linkbase}updater.py", "updater.py")
+    save_link_as_file(f"{linkbase}gui.py", "gui.py")
+    save_link_as_file(f"{linkbase}utils.py", "utils.py")
